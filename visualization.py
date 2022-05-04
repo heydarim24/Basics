104655e6f84668fae52e4678ec101a73b43df24e
heydarim24 <104922548+heydarim24@users.noreply.github.com>
2022-05-04 09:49:15 -0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -8,3 +8,15 @@ x = [1, 2, 3]
 y = [1, 4, 9]
 plt.plot(x, y) 
 plt.show()
+
+
+sample_data = pd.read_csv('Sample_Data.csv')
+sample_data
+type(sample_data)
+#retrieve column 
+sample_data.columnC.iloc[1] #get the second element 
+
+plt.plot(sample_data.columnA, sample_data.columnB)
+plt.show
+
+
